364b277fbd58782729430a7bbd88059df3e37159
ntwck <158731522+ntwck@users.noreply.github.com>
2024-03-19 23:18:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,9 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/ntwck/openwrt-packages' feeds.conf.default
+sed -i '$a src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' feeds.conf.default
 
 #sed -i '$a src-git packages https://git.openwrt.org/feed/packages' feeds.conf.default
 #sed -i '$a src-git luci https://git.openwrt.org/project/luci' feeds.conf.default
